d67c2f3c156ec7635cc0f1b702e4b7bbf95740f7
helena.sanjaya@binus.ac.id <AS24-2@slc.net>
2026-02-23 16:51:25 +0700

    fix: deepcopy model before INT8 PTQ

--- src/quantization/ptq/int8.py
+++ src/quantization/ptq/int8.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 from src.config import QUANTIZATION_DTYPE, TARGET_LAYERS
 
@@ -5,5 +6,5 @@ from src.config import QUANTIZATION_DTYPE, TARGET_LAYERS
 class INT8Quantizer:
     @staticmethod
     def quantize(model):
-        model_cpu = model.cpu()
+        model_cpu = copy.deepcopy(model).cpu()
         return torch.quantization.quantize_dynamic(model_cpu, TARGET_LAYERS, dtype=QUANTIZATION_DTYPE)
